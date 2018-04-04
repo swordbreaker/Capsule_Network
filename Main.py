5aef16e4b12b75d54b8a2e1c304b6168e89415e6
Tobias Bollinger <tobias.bollinger@students.fhnw.ch>
2018-04-04 16:16:32 +0200

    Trained the mnsit fashion data set

--- Main.py
+++ Main.py
@@ -3,8 +3,8 @@ from Examples import *
 
 tf.logging.set_verbosity(tf.logging.ERROR)
 
-#mnist_fashion(epochs=3, train=True, eval=True)
+mnist_fashion(epochs=5, train=True, eval=True)
 
 #mnist(train=False, eval=False)
 
-mushroom_example(train=True, eval=True, epochs=4)
+#mushroom_example(train=True, eval=True, epochs=4)
